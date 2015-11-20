fc6893744b7be2346107f1ff345f63f46fa266e1
Eric <shl1018@hotmail.com>
2015-11-20 09:49:26 +0800

    init commit

---
+++ UIButton_CODE/UIButton_CODE/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  UIButton_CODE
+//
+//  Created by imac on 11/20/15.
+//  Copyright © 2015 Eric. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
