dceeb38a1fe77389326fda1cdccaeeed609a5af8
Eric <shl1018@hotmail.com>
2015-11-20 10:19:02 +0800

    commit

---
+++ UIButton_CODE/UIButton_CODE/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UIButton_CODE
+//
+//  Created by imac on 11/20/15.
+//  Copyright © 2015 Eric. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UIButton_CODE/UIButton_CODE/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UIButton_CODE
+//
+//  Created by imac on 11/20/15.
+//  Copyright © 2015 Eric. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
